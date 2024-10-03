6b5df2e9cf9153afa7723f021d4e6b1050fa2fdc
habiburrahmanriaz <habiburriaz2@gmail.com>
2024-10-04 01:59:55 +0600

    Slider add
    Co-authored-by: MD JUBAYER AHMAD <Jubayerahmaad@users.noreply.github.com>

---
+++ app.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".swiper-content", {
+      slidesPerView: 3,
+      spaceBetween: 30,
+      
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
